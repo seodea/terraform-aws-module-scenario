3488ee23f040833621c6d1bb4e4d90938a5f3018
서대현 <sdh@megazone.com>
2022-07-26 13:27:44 +0900

    test

--- service/vpc/main.tf
+++ service/vpc/main.tf
@@ -2,7 +2,7 @@
 
 module "vpc" {
     #source = "../../module/vpc"
-    source = "git::https://github.com/seodea/AWS_tf_module_scenario//module/vpc.git?ref=master"
+    source = "git@github.com:seodea/terraform-AWS-module-scenario.git"
 
     company = var.company
 
