cc798ca51f5a7910143296d767f96ac2e429471d
MZC01-SDH <mzc01-sdh@MZC01-SDH.local>
2024-09-09 13:17:41 +0900

    upload ec2 instance module

---
+++ module/terraform-aws-ec2-instance/ec2.tf
@@ -0,0 +1,50 @@
+############################
+# EC2 instance
+############################
+
+resource "aws_instance" "this" {
+
+  ami                  = var.ami
+  instance_type        = var.instance_type
+
+  user_data              = var.user_data
+
+  availability_zone      = var.azs #  
+  subnet_id              = join("",data.aws_subnets.this.ids) # string : var.subnet 
+  vpc_security_group_ids = data.aws_security_groups.this.ids # list 형식
+
+  key_name             = var.key_name # string
+  monitoring           = var.monitoring # 설정을 안할 경우 null
+  
+  iam_instance_profile = var.create_iam_instance_profile ? 1 : var.iam_instance_profile # 설정을 안할 경우 null
+
+  associate_public_ip_address = var.associate_public_ip_address # dafault 값은 IP 할당 X
+  private_ip                  = var.private_ip # 설정을 안할 경우 null
+
+  # root disk만 여기서 진행, 추가 Disk는 disk attachment로 진행
+  dynamic "root_block_device" {
+    for_each = local.root_block_device
+
+    content {
+      delete_on_termination = try(root_block_device.value.delete_on_termination, null)
+      encrypted             = try(root_block_device.value.encrypted, null)
+      iops                  = try(root_block_device.value.iops, null)
+      kms_key_id            = lookup(root_block_device.value, "kms_key_id", null)
+      volume_size           = try(root_block_device.value.volume_size, null)
+      volume_type           = try(root_block_device.value.volume_type, null)
+      throughput            = try(root_block_device.value.throughput, null)
+      tags                  = try(root_block_device.value.tags, null)
+    }
+  }
+
+  tags        = merge(
+    var.tags, 
+    { "Name" = format("%s-%s-%s-ec2",
+        var.company,
+        var.env,
+        var.method
+      )
+    }
+  )
+  # volume_tags = var.enable_volume_tags ? merge({ "Name" = var.name }, var.volume_tags) : null
+}
