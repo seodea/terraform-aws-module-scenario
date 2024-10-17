254d1d28d9adc8df45df884716cb95ebf3696ef9
MZC01-SDH <mzc01-sdh@MZC01-SDH.local>
2024-10-17 15:13:41 +0900

    update route.tf file

--- module/terraform-aws-route/route.tf
+++ module/terraform-aws-route/route.tf
@@ -13,12 +13,15 @@ resource "aws_route_table" "this" {
 }
 
 resource "aws_route" "this" {
-  for_each = { for route in var.route_rule : route.cidr_block => route }
+  # for_each = { for route in var.route_rule : route.cidr_block => route } #change var.route_rule -> local.route_rule
+  for_each = { for route in local.route_rule : route.name => route } #change var.route_rule -> local.route_rule
 
   route_table_id         = aws_route_table.this.id
-  # destination_cidr_block = each.key
-  destination_cidr_block     = can(regex("^[0-255]", each.key)) == true ? each.key : null
-  destination_prefix_list_id = can(regex("^pl-", each.key)) == true ? each.key : null
+  # destination_cidr_block     = can(regex("^[0-255]", each.key)) == true ? each.key : null
+  # destination_prefix_list_id = can(regex("^pl-", each.key)) == true ? each.key : null
+  
+  destination_cidr_block     = can(regex("^[0-255]", each.value.cidr_block)) == true ? each.key : null
+  destination_prefix_list_id = can(regex("^pl-", each.value.cidr_block)) == true ? each.key : null
 
   gateway_id                 = startswith(each.value.target, "igw-") == true ? each.value.target : null
   nat_gateway_id             = startswith(each.value.target, "nat-") == true ? each.value.target : null
