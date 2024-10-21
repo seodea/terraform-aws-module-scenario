5896e1648df49486a8217c1ddfc13780604e7f1c
MZC01-SDH <mzc01-sdh@MZC01-SDH.local>
2024-10-21 10:47:22 +0900

    modify files for  attachement ebs #13

--- module/terraform-aws-ec2-instance/ec2.tf
+++ module/terraform-aws-ec2-instance/ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "this" {
   ami                         = var.ami
   instance_type               = var.instance_type
 
-  user_data                   = file("${path.module}/${var.user_data}")
+  user_data                   = var.user_data == null ? null : file("${path.module}/${var.user_data}")
 
   availability_zone           = var.azs #  
   subnet_id                   = join("",var.subnet) # string : var.subnet 
